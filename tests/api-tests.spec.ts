a699db752cbaf157a3a8eabb51aa5983f876ec46
Hareef <hareef.dadabhaigari@tecra.com>
2026-06-29 17:09:45 +0530

    testing

--- tests/api-tests.spec.ts
+++ tests/api-tests.spec.ts
@@ -31,30 +31,45 @@ test.skip('DELETE /api/data should return 200', async ({ request }) => {
 	expect(response.status()).toBe(200);
 });
 
+// test('GET /api/data should return 200', async ({ request }) => {
+// 	const response = await request.get('https://conduit-api.bondaracademy.com/api/tags');
+// 	const responseBody = await response.json();
+// 	// console.log('Response Body:', responseBody);
+// 	 expect(responseBody.tags[0]).toBe("Test");
+// 	 expect(responseBody.tags).toHaveLength(10);
+// });
+
 test('GET /api/data should return 200', async ({ request }) => {
-	const response = await request.get('https://conduit-api.bondaracademy.com/api/tags');
+	const response = await request.get('tags');
+
+	expect(response.status()).toBe(200);
+
 	const responseBody = await response.json();
-	// console.log('Response Body:', responseBody);
-	await expect(responseBody.tags[0]).toBe("Test");
-	await expect(responseBody.tags).toHaveLength(10);
+
+	expect(responseBody.tags[0]).toBe('Test');
+	expect(responseBody.tags).toHaveLength(10);
 });
 
 test('POST /api/data should return 201', async ({ request }) => {
-	const response = await request.post('https://conduit-api.bondaracademy.com/api/articles/',
-		{
-			headers: {
-				Authorization: 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo1NjcyMX0sImlhdCI6MTc4MjMwOTk1NywiZXhwIjoxNzg3NDkzOTU3fQ.G2OW_GARNbYpuGOK2_5LKpS7h8k24-RB2cMI0tWOVzo'
-			},
-			data: {
-				"article": {
-					"title": "123131111",
-					"description": "Description121",
-					"body": "Hello this body1",
-					"tagList": ["tag311", "tag231"]
-				}
+	const currentData = new Date();
+	const response = await request.post('articles', {
+		headers: {
+			Authorization: 'Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo1NjcyMX0sImlhdCI6MTc4MjMwOTk1NywiZXhwIjoxNzg3NDkzOTU3fQ.G2OW_GARNbYpuGOK2_5LKpS7h8k24-RB2cMI0tWOVzo'
+		},
+		data: {
+			"article": {
+				"title": `${currentData.getTime()}`,
+				"description": "Description",
+				"body": "Hello this body1",
+				"tagList": ["youtube", "facebook", "twitter"]
 			}
 		}
+	}
 	);
-	await response.json();
-	// console.log('Response Body:', responseBody);
+	const responseBody = await response.json();
+	expect(response.status()).toBe(201);
+	expect(responseBody.article).toHaveProperty('title', `${currentData.getTime()}`);
+	expect(responseBody.article.description).toBe('Description');
+	const tagList = responseBody.article.tagList;
+	//console.log('Response Body:', responseBody);	
 });
